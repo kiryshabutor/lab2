868f5b225a4a65993b6d94ffcba22d05f1de5100
kiribu <kiryshabutor@gmail.com>
2025-09-17 19:17:10 +0300

    Initial commit

---
+++ includes/app.h
@@ -0,0 +1,10 @@
+#pragma once
+#include "../includes/string.h"
+
+class App {
+  public:
+    App() = default;
+    ~App()= default;
+
+    void run() const;
+};
---
+++ src/main.cpp
@@ -0,0 +1,8 @@
+#include "../includes/app.h"
+
+int main() {
+
+    App app;
+    app.run();
+    return 0;
+}
